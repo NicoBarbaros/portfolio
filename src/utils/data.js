f51f10e4fe1e9f215af92e70dd7f2ace444587b2
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-05-26 17:05:05 +0300

    export social media links as an object

---
+++ src/utils/data.js
@@ -0,0 +1,14 @@
+
+const footerLinks = [
+	{
+		title: 'github',
+		link: 'https://github.com/nicubarbaros'
+	},
+
+	{
+		title: 'facebook',
+		link: 'https://www.facebook.com/BarbarosNicolae'
+	}
+];
+
+export { footerLinks };
