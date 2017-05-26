27393a8c39a38fc5a58386d39fb9d6df0c5df018
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-05-26 17:07:05 +0300

    feat(footer): add account links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react';
 
+import Links from '../Links/index';
 import style from './style.css';
 
 import weekday from '../../utils/weekday';
+import {footerLinks as links} from '../../utils/data';
 
 class Footer extends Component {
+
+
 	render () {
 		return (
 			<div>
 			<p>Have a nice {weekday()}</p>
+			<Links links={links}/>
 			</div>
 		)
 	}
