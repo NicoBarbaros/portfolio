a64bac093c528837492e4da0a09ff98089a2d942
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-05-27 16:00:33 +0300

    feat(containers): add container text

---
+++ src/containers/Text/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './style.css';
+
+class Text extends Component {
+		render() {
+				return (
+						<p>{this.props}</p>
+				)
+		}
+}
+
+export default Text;
