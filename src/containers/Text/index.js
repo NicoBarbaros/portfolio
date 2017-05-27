b7fa4609871d1cb52033ed0083f54460919cc99b
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-05-27 20:21:51 +0300

    feat(containers): setup high order component text

---
+++ src/containers/Text/index.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import PropTypes from 'prop-types';
+
+class Text extends Component {
+
+		componentDidMount () {
+				if(this.refs.section) {
+						let links = this.refs.section.querySelectorAll('a');
+
+						console.log(links);
+						for(let i = 0; i < links.length; i++) {
+								links[i].addEventListener('mouseover', this.handleMouseOver);
+						}
+				}
+		}
+
+		handleMouseOver (event) {
+				// efectText(event.target)
+				// Do something with the text when you hover
+		}
+
+		render () {
+				const {data} = this.props;
+
+				const dataItems = data.map((element, index) =>
+						<div key={index.toString()}>
+								{element}
+						</div>
+				);
+
+				return (
+						<section ref="section">
+								{dataItems}
+						</section>
+				)
+		}
+}
+
+Text.propTypes = {
+		data: PropTypes.array.isRequired
+}
+
+export default Text;
+
