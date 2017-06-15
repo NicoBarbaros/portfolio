dc96fd23d751ecc588e6dc63d0e7ac061425d6ba
Barbaros Nicolae <barbarosnico@MacBook-Pro.local>
2017-06-15 23:49:31 +0300

    update random word in ui at a specific interval

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,30 +1,54 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Links from '../Links/index';
-import style from './style.css';
+import style from '../../assets/stylesheets/base.scss';
 import {withRouter} from 'react-router-dom';
+import classnames from 'classnames';
 
 import weekDay from '../../utils/weekday';
 import {footer as links} from '../../utils/data';
 import getUniqueWord from '../../utils/words';
 
 class Footer extends Component {
-	constructor(props) {
-		super(props);
+	constructor (props) {
+		super (props);
+
+		this.state = {
+			word: 'amazing'
+		}
+	}
+
+	componentDidMount () {
+		this.interval = setInterval(
+		 () => this.unique(),
+		 5000
+		);
+	}
+
+	componentWillUnmount () {
+		if(this.interval) clearInterval(interval);
+	}
+
+	unique () {
+		this.setState(() => ({ word: getUniqueWord() }));
 	}
 
 	render () {
-			const { location } = this.props;
+		const { location } = this.props;
 
-			return (
-					<footer className={style.footer}>
-							{location.pathname === '/' &&
-							<p>Enjoy {getUniqueWord()} {weekDay()}</p>
-							}
-						 <Links links={links}/>
-				</footer>
-			)
-		}
+		return (
+			<footer className={classnames(style.footer)}>
+				<div className={style.mainContainer}>
+						{location.pathname === '/' &&
+							<div class={classnames(style.text, style.textParagraph)}>
+								<p>Enjoy {this.state.word} {weekDay()}</p>
+							</div>
+						}
+					 <Links links={links}/>
+				</div>
+			</footer>
+		)
+	}
 }
 
 //
